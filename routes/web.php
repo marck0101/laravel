40ef899d64084df6a1ee1d308791ce6d0346cc11
marck0101 <marcos.henrique.a7x@gmail.com>
2022-10-05 07:57:43 -0300

    laravel, curso matheus Battisti

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,13 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProdutoController;
 
 // algumas versões funciona assim
-// Route::get('/', 'HomeController@index');
+Route::get('/', 'HomeController@index');
 
 // Mas a que eu tenho instalada a sintexe precisa ser assim
-Route::get('/', [HomeController::class, 'index']);
+// Route::get('/', [HomeController::class, 'index']);
 
-Route::get('/produtos', [ProdutoController::class, 'index']);
+// Route::get('/produtos', [ProdutoController::class, 'index']);
 
-Route::post('/produtos', [ProdutoController::class, 'index']);
-Route::post('/produtos/excluir{id}', [ProdutoController::class, 'index']);
+// Route::post('/produtos', [ProdutoController::class, 'index']);
+
+// Route::get('/produtos/excluir{id}', [ProdutoController::class, 'excluir']);
