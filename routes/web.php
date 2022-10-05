4aabb4a07e0fc7c047dd347030b8b5a8f3eff7b1
marck0101 <marcos.henrique.a7x@gmail.com>
2022-10-05 09:16:26 -0300

    passando parametros na rota para usar na view, usando for, forEach e impondo condicoes

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     $nome = 'Marcos';
-
-//     return view('welcome', ['nome' => $nome]);
-// });
-
+//para cada view vou ter que criar e passar
+// os dados que eu quero que ela receba
 Route::get('/', function () {
     $nome = 'Marcos';
+    $arr = [10, 20, 30, 40, 50];
+    $arrNomes = [
+        "Roberto",
+        "Lucas",
+        "Augusto"
+    ];
 
-    return view('welcome2', ['nome' => $nome]);
+    return view(
+        'welcome',
+        [
+            'nome' => $nome,
+            'arr' => $arr,
+            'arrNomes' => $arrNomes
+        ]
+    );
 });
